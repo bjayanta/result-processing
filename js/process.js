5d9191e9724e3802ec3311655a197e6f3dfe3349
Jayanta Biswas <bjayanta.me@gmail.com>
2019-04-07 17:44:20 +0600

    change option and process

--- js/process.js
+++ js/process.js
@@ -1,50 +1,66 @@
 (function($) {
+    /**
+     * define plugin name
+     * set default options and set rules
+     * catch mark from element data attribute 
+     * initialize the result object
+     * 
+     * check the given mark is 'greater then or equal' from passmark
+     * match the mark to the given settings
+     * 
+     * set the result into dom
+     * set the success method and send the result object as param
+     */
 
-    $.fn.result = function(options) {
+    // define plugin name
+    $.fn.calculateResult = function(options) {
+        // set default options and set rules
         var settings = $.extend({
+            marks: [80, 70, 60, 50, 40, 33],
+            grades: ['A+', 'A', 'A-', 'B', 'C', 'D'],
+            points: [5.00, 4.00, 3.50, 3.00, 2.00, 1.00, 0.00],
             passmarks: 33,
             success: null
         }, options);
 
+        // return this for method chaining 
         return this.each(function() {
             var $this = $(this);
-            var mark = $this.data('mark'),
-                grade = '',
-                point = 0.00;
 
-            if(mark >= 80) {
-                grade = 'A+';
-                point = 5.00;
-            } else if(mark >= 70) {
-                grade = 'A';
-                point = 4.00;
-            } else if(mark >= 60) {
-                grade = 'A-';
-                point = 3.50;
-            } else if(mark >= 50) {
-                grade = 'B';
-                point = 3.00;
-            } else if(mark >= 40) {
-                grade = 'C';
-                point = 2.00;
-            } else if(mark >= 33) {
-                grade = 'D';
-                point = 1.00;
-            } else {
-                grade = 'F';
-                point = 0.00;
+            // get mark and set default result object 
+            var mark = $this.data('mark'),
+                result = {
+                    grade: '',
+                    point: 0.00,
+                    mark: 0.00
+                };
+            
+            // check the mark 'greater then or equal' from passmark 
+            if(mark >= settings.passmarks) {
+                // make a itaration for every rules
+                for(i in settings.marks) {
+                    // find the grade and point using rules
+                    if(mark >= settings.marks[i]) {
+                        // assign result object 
+                        result.grade = settings.grades[i];
+                        result.point = settings.points[i];
+                        result.mark = mark;
+
+                        // if match brak the itaration 
+                        break;
+                    }
+                }
+            } else { // set result for lowest mark
+                result.grade = 'F';
+                result.point = 0.00;
+                result.mark = mark;
             }
 
-            $this.text("Mark: " + mark + ", Grade: " + grade + ", Point: " + point);
+            // set dom 
+            $this.text("Mark: " + result.mark + ", Grade: " + result.grade + ", Point: " + result.point);
 
+            // call the success method
             if($.isFunction(settings.success)) {
-                var result = {
-                    mark: mark,
-                    grade: grade,
-                    point: point
-                };
-
-                // console.log(result);
                 settings.success.call(this, result);
             }
         });
